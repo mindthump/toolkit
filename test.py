413f67f572504ca5274670bc561d26fc99466510
Ed Cardinal <edcgit@mindthump.com>
2021-09-03 16:16:27 -0700

    Disable pudb.

--- test.py
+++ test.py
@@ -1,7 +1,10 @@
 # import pudb
 
+# NOTE: I use this for python development so I include the 'pudb' console
+# debugger (currently commented out). See https://pypi.org/project/pudb/
+
 foo = 123
 bar = "splunge"
-pu.db
-baz = "Variables: {} and {}".format(foo, bar)
-print(baz)
+# Breakpoint in pudb
+# pu.db
+print(f"Variables: {foo} and {bar}")
